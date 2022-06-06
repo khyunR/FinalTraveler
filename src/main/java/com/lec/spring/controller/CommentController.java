ae514c4c47967e32e70b41756b48698cc27b2166
soohyune99 <soohyune1211@gmail.com>
2022-06-06 23:28:57 +0900

    comment

--- src/main/java/com/lec/spring/controller/CommentController.java
+++ src/main/java/com/lec/spring/controller/CommentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lec.spring.domain.CommentDTO;
 import com.lec.spring.service.CommentService;
 
 
@@ -24,6 +25,12 @@ public class CommentController{
 	@RequestMapping("/roomDetail")
 	public String roomDetail(Model model) {
 		model.addAttribute("list", commentService.list());
+		return "/room/roomDetail";
+	}
+	
+	@RequestMapping("/writeReviewOk")
+	public String writeReviewOk(Model model, CommentDTO dto ) {
+		model.addAttribute("list", commentService.write(dto));
 		return "/room/roomDetail";
 	}
 }	
